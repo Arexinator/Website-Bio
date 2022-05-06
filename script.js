538fe0b323bb8a5a009dce3e0ebbc0ae0335d299
Arexinator <74128329+Arexinator@users.noreply.github.com>
2022-05-06 13:31:40 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Website Dev!","Discord Bot Dev!","14 yo Teenager!","Gamer!"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true
+    })
+});
