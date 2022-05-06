19356cb9c47c24f4474ea39ea33858c31023ae07
Arexinator <74128329+Arexinator@users.noreply.github.com>
2022-05-06 14:10:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var typed = new Typed(".typing", {
-        strings: ["Website Dev!","Discord Bot Dev!","Minecrafter!"],
+        strings: ["Website Dev!","Discord Bot Dev!", "Server Dev", "Minecrafter!"],
         typeSpeed: 80,
         backSpeed: 50,
         loop: true
