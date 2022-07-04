dfaa711cabadb20f775478ee752d55e53c03316b
Arex <74128329+Arexinator@users.noreply.github.com>
2022-07-04 17:12:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["","Discord Bot Developer.",'Website Developer.',"Server Owner."],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true
+    })
+});
